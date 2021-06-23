45b0ad51493da3942add53cb06b2940328e20901
wwjdtm <jje0ng@kookmin.ac.kr>
2021-06-24 00:15:35 +0900

    vue project

---
+++ vue3-project/src/App.vue
@@ -0,0 +1,38 @@
+// 컴포넌트는 하나의 부품 ~ 컴포넌트 만들때 확장자 .vue 로 들어감 
+
+//templat 안에 div 들어감
+<template>
+  <!-- <div class="name">{{ name }}</div> -->
+  <div class="name">{{ great }}</div>
+
+</template>
+
+// 자바스크립트 작성
+<script>
+export default {
+  setup() {
+    const name = 'hussey Kim';
+
+    const greeting = (name) => {
+      return 'Hello ' + name;
+    };
+
+    const great = greeting(name);
+
+    //원하는 변수 리턴을 해줘야 template 에서 접근이 가능해짐 
+    return {
+      great
+    };
+  }
+  
+}
+</script>
+
+
+//css 코드
+<style>
+  .name {
+    color : blue;
+  }
+
+</style>
---
+++ vue3-project/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+//최상위 컴포넌트를 id 가 app인 div 에 넣어줌 
+createApp(App).mount('#app')
