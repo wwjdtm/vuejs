399c1ba4ec78e02f2ffd58d40d89ae0727b2adda
wwjdtm <jje0ng@kookmin.ac.kr>
2021-07-12 01:38:18 +0900

    add components

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,98 +1,51 @@
 <template>
-    <div class="container">
+  <div class="container">
+    <h2>To-Do List</h2>
+    <TodoSimpleForm @add-todo="addTodo" />
 
-        <h2>To-Do List</h2>
-        <!-- submit.prevent 리프레쉬하는 속성을 막음 -->
-        <form @submit.prevent = "onsubmit" >
-            <div class="d-flex">
-                <div class="flex-grow-1 mr-2">
-                    <input class = "form-control " type="text" v-model="todo" placeholder="Type new to-do">
-                </div>
-                <div>
-                    <button class="btn btn-primary" type = "submit">Add</button>
-                </div>
-
-            </div>
-            
-
-            <div v-show="hasError" style="color: red">field empty</div>
-           
-           
-            
-        </form>
-        <div v-if="!todos.length">추가된 todo 가 없습니다</div>
-        <!-- v-for 사용하려면 고유값 있어야됨 .-> key : todo id -->
-        <div v-for = "(todo, index) in todos" class="card mt-2" :key="todo.id">
-            <div class="card-body p-2 d-flex align-items-center">
-                <div class = "form-check flex-grow-1">
-                    <input class = "form-check-input" type="checkbox" v-model="todo.completed">
-                    <label class = "form-check-label" :style="todo.completed ? todoStyle : {}" > {{ todo.subject }}</label>
-                </div>
-
-                <div>
-                    <button class = "btn btn-danger btn-sm"
-                    @click = "deleteTodo(index)"> Delete</button>
-                </div>
-              
-
-            </div>
-
-        </div>
-    </div>
-    
+    <div v-if="!todos.length">추가된 todo 가 없습니다</div>
+    <!-- props로 보내는 이름을 적고 데이터명 적음 -->
+    <!-- 자식 컴포넌트에서는 앞의 이름으로 뒤의 데이터에 접근가능 -->
+    <TodoList :todos="todos" />
+  </div>
 </template>
 
 <script>
 import { ref } from 'vue';
+import TodoSimpleForm from '@/components/TodoSimpleForm.vue';
+import TodoList from '@/components/TodoList.vue';
 
-export default({
-    setup() {
-        const todo = ref('');
-        const todos = ref([
-            // {id : 1, subject : 'ee'}
-            ]);
+export default {
+  components: {
+    TodoSimpleForm,
+    TodoList,
+  },
+  setup() {
+    const todos = ref([
+      // {id : 1, subject : 'ee'}
+    ]);
 
-        const hasError = ref(false);
-        const todoStyle = {
-            textDecoration : 'line-through',
-            color : 'gray'
-        }
+    const todoStyle = {
+      textDecoration: 'line-through',
+      color: 'gray',
+    };
 
+    const addTodo = (todo) => {
+      todos.value.push(todo);
+    };
 
-        const onsubmit = () => {
-            if ( todo.value ===''){
-                hasError.value = true;
-            } else {
-                
-                todos.value.push({
-                id : Date.now(),
-                subject : todo.value,
-                completed : false
-            });
-            hasError.value = false;
-            todo.value = '';
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
+    };
 
-            }
-            
-        }
-
-        const deleteTodo = (index) => {
-            todos.value.splice(index, 1);
-        }
-    
-        return {
-            todo,
-            onsubmit,
-            todos,
-            hasError,
-            todoStyle,
-            deleteTodo
-        };
-    },
-    
-})
+    return {
+      addTodo,
+      todos,
+      todoStyle,
+      deleteTodo,
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
